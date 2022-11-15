5d8430817aff9928e0e9fe6b7ab032db55c6a35d
Anish <21b090003@iitb.ac.in>
2022-11-15 16:48:18 +0530

    Working input

--- Anish/client.py
+++ Anish/client.py
@@ -1,31 +1,42 @@
 from sys import argv
 import socket
 import json
+import threading
 
 if len(argv) < 3:
     print(f"Usage: {argv[0]} <host> <port>")
 
 id = int(argv[3])
-rid = int(argv[4])
-
 
 server_addr = (argv[1], int(argv[2]))
 
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_sock.connect(server_addr)
 
-di = {"id":id, "mssg":"Anish","recipient":rid}
-msg=json.dumps(di)
+
+def listen():
+    data = client_sock.recv(1024)
+    print("Recieved:")
+    while data:
+        print(json.loads(data.decode("UTF-8"))["mssg"])
+        data=client_sock.recv(1024)
+
+    client_sock.close()
 
 
-#client_sock.sendall(b"Hello networking world\n")
+t1 = threading.Thread(target=listen)
 
-client_sock.sendall(bytes(msg, encoding="utf-8"))
-data = client_sock.recv(1024)
+t1.start()
 
-print("Recieved:")
-while data:
-    print(json.loads(data.decode("UTF-8"))["mssg"])
-    data=client_sock.recv(1024)
 
-client_sock.close()
+while True:
+    x = input()
+    u = x.find(':')
+    rid = int(x[0:u])
+    mssg = x[u+1:]
+    di = {"id":id, "mssg":mssg,"recipient":rid}
+    msg=json.dumps(di)
+    client_sock.sendall(bytes(msg, encoding="utf-8"))
+
+
+
