954afe8b9e4933eca06ee5b6e48e3c6ebacc24f7
HehBot <razor7827@gmail.com>
2022-11-25 18:04:23 +0530

    Fixed latency eval script

--- test/find_latency.py
+++ test/find_latency.py
@@ -3,26 +3,34 @@ num_files = int(argv[1])
 num = int(argv[2])
 avg = 0
 for i in range(num_files):
-    file_name = "client"+str(i+1)+"/client"+str(i+1)+".out"
+    file_name = "client" + str(i + 1) + "/client" + str(i + 1) + ".out"
     f = open(file_name,'r')
     lines = f.readlines()
-    count = 1
+    count = -1
     tot = 0
     tot_time = 0
-    for line in lines:
-        if count>num*6:
-            if count%6==3:
-                csec = float(line[-8:])
-                cmin = float(line[-11:-9])
-                time = (60*cmin+csec)
-            elif count%6==4:
-                csec = float(line[-8:])
-                cmin = float(line[-11:-9])
-                time = time - 60*cmin-csec
-                tot = tot+1
-                tot_time = tot_time+time
-        count = count+1
 
-    avg = avg+(tot_time/tot)
+    recv_time = None
+    onb_time = (60 * float(lines[0][-11:-9]) + float(lines[0][-8:]))
+
+    lines = lines[1:]
+
+    for line in lines:
+        count = count + 1
+        if count % 6 == 1:
+            csec = float(line[-8:])
+            cmin = float(line[-11:-9])
+            recv_time = (60 * cmin + csec)
+        elif count % 6 == 2:
+            csec = float(line[-8:])
+            cmin = float(line[-11:-9])
+            sent_time = 60 * cmin + csec
+            if onb_time > sent_time:
+                continue
+            time = recv_time - sent_time
+            tot = tot + 1
+            tot_time = tot_time + time
+
+    avg = avg + (tot_time/tot)
 
 print(avg/num_files)
