1ded70881c8a9ba0497186591986acaf15069ade
Anish <21b090003@iitb.ac.in>
2022-11-25 21:41:42 +0530

    Added balancing server to server folder

--- test/find_latency_throughput.py
+++ test/find_latency_throughput.py
@@ -1,7 +1,7 @@
 from sys import argv
 
 num_files = int(argv[1])
-
+arr=[]
 avg = 0
 
 for i in range(num_files):
@@ -13,25 +13,28 @@ for i in range(num_files):
     tot_time = 0
 
     recv_time = None
-    onb_time = (60 * float(lines[0][-10:-8]) + float(lines[0][-7:]))
+    onb_time = (60 * float(lines[0][-11:-9]) + float(lines[0][-8:]))
 
     lines = lines[1:]
 
     for line in lines:
         count = count + 1
         if count % 6 == 1:
-            csec = float(line[-7:])
-            cmin = float(line[-10:-8])
+            csec = float(line[-8:])
+            cmin = float(line[-11:-9])
             recv_time = (60 * cmin + csec)
         elif count % 6 == 2:
-            csec = float(line[-7:])
-            cmin = float(line[-10:-8])
+            csec = float(line[-8:])
+            cmin = float(line[-11:-9])
             sent_time = 60 * cmin + csec
             if onb_time > sent_time:
                 continue
             time = recv_time - sent_time
             tot = tot + 1
+            arr.append(time)
             tot_time = tot_time + time
 
     avg = avg + (tot_time/tot)
 print("Average latency = " + str(1000 * avg/num_files) + " ms")
+arr.sort()
+print(arr)
